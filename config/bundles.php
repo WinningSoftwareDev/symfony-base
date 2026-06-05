55d12ac37ae1d61a0166f63533948f37921b1e42
Daniel Winning <daniel@winningsoftware.co.uk>
2026-06-05 23:07:45 +0100

    Add liip health monitor checks

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,5 @@ return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    \Liip\MonitorBundle\LiipMonitorBundle::class => ['all' => true],
 ];
